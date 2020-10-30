2099ecb0a8ce173c2258874f3261ed9f2f355ec3
Deeds12 <svigirn.@gmail.com>
2020-10-30 16:55:59 +0100

    s10

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+var js  = 'amazing';
+console.log(40+8+7-4);
+
+console.log("Nikolaaaaa");
+
+var firstName = "Nikola";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+var myFirstJob = "Student";
+var myCurrentJob = "Programmer";
+
+console.log(myFirstJob);
